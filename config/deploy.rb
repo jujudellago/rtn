7b7aa298c0a95cff127a5e608ffd5eaf6714e519
Julien Ramel <julien.ramel@gmail.com>
2018-08-08 13:54:12 +0200

    added cache to bedrock

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :log_level, :info
 
 # Apache users with .htaccess files:
 # it needs to be added to linked_files so it persists across deploys:
-set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
+set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess', 'web/app/db.php', 'web/app/object-cache.php', 'web/app/advanced-cache.php')
 #set :linked_files, fetch(:linked_files, []).push('.env')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 
