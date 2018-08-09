c7b3708e2f16e52b99d0b49fe12a8f2797dbd64f
Julien Ramel <julien.ramel@gmail.com>
2018-08-09 09:14:43 +0200

    added fb root

--- web/app/themes/bridge/header.php
+++ web/app/themes/bridge/header.php
@@ -30,7 +30,14 @@
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.1&appId=270180300377448&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <?php
 $params = qode_header_parameters();
 extract($params);
